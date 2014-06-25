7e0b07504c0ff0d9dcfa756ed979bf495eaa6496
user <user@C9>
2014-06-25 17:33:21 +0200

    First init Starting a new repo for generic Guis such as AtkPanel, Jive and Astor. Contains all necessary jar libraries for stand-alone programs Jive has a batch file for starting it on  Windows, the other programs have not (yet)

---
+++ jive.bat
@@ -0,0 +1,8 @@
+:: @echo off
+:: ---------------------------------------------------------
+
+@set TANGO_HOST=172.25.65.1:10000
+
+:: java -mx128m -DTANGO_HOST=%TANGO_HOST% fr.esrf.tangoatk.widget.jdraw.SimpleSynopticAppli panels/Ueberwachung.jdw
+:: java -mx128m -DTANGO_HOST=%TANGO_HOST% -cp %LIB_DIR% fr.esrf.tangoatk.widget.jdraw.SimpleSynopticAppli panels/Ueberwachung.jdw
+java -mx128m -DTANGO_HOST=%TANGO_HOST% -cp TangORB.jar;ATKCore.jar;ATKWidget.jar;atkpanel.jar;LogViewer.jar;Jive.jar;log4j.jar jive3.MainPanel
