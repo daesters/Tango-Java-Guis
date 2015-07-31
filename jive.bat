fbc64ae78d3a8d88df0a73055b4fdff942c8ea8f
user <user@spannort>
2015-07-31 12:10:08 +0200

    Adapted jive.bat file to work with libs folder

--- jive.bat
+++ jive.bat
@@ -2,7 +2,8 @@
 :: ---------------------------------------------------------
 
 @set TANGO_HOST=172.25.65.1:10000
+@set LIBPATH=libs\
 
 :: java -mx128m -DTANGO_HOST=%TANGO_HOST% fr.esrf.tangoatk.widget.jdraw.SimpleSynopticAppli panels/Ueberwachung.jdw
 :: java -mx128m -DTANGO_HOST=%TANGO_HOST% -cp %LIB_DIR% fr.esrf.tangoatk.widget.jdraw.SimpleSynopticAppli panels/Ueberwachung.jdw
-java -mx128m -DTANGO_HOST=%TANGO_HOST% -cp TangORB.jar;ATKCore.jar;ATKWidget.jar;atkpanel.jar;LogViewer.jar;Jive.jar;log4j.jar jive3.MainPanel
+java -mx128m -DTANGO_HOST=%TANGO_HOST% -Djava.library.path=%LIBPATH%  -cp %LIBPATH%TangORB.jar;%LIBPATH%ATKCore.jar;%LIBPATH%ATKWidget.jar;%LIBPATH%Atkpanel.jar;%LIBPATH%LogViewer.jar;%LIBPATH%Jive.jar;%LIBPATH%log4j.jar jive3.MainPanel
