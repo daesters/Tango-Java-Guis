e61fa06620e878c99d6b1911d238630bdd89e5b5
windows7-64bit <devnull@localhost>
2017-02-13 15:47:36 +0100

    Cleand up Install script On next commit, we will try adapted (aka path independent) installation of desktop files

---
+++ DesktopFiles/Jive.desktop.tpl
@@ -0,0 +1,12 @@
+[Desktop Entry]
+Name=Jive
+Exec=<execpath>/StartJive.sh
+Icon=/usr/share/icons/custom/Jive.png
+Type=Application
+OnlyShowIn=GNOME;Unity;
+
+
+[Desktop Action New]
+Name=New Gui
+Exec=<execpath>/StartJive.sh
+OnlyShowIn=GNOME;Unity;
--- DesktopFiles/install.sh
+++ DesktopFiles/install.sh
@@ -6,24 +6,35 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
 
+APPLICATIONSDIR=/usr/share/applications
+# Default path in .desktop files
+DEFAULTSCRIPTPATH='\/home\/geissepeter\/Desktop\/TangoGuis\/GenericGuis'
+
 if [ ! -d "/usr/share/icons/custom" ]; then
   echo "Directory does not exist"
   mkdir /usr/share/icons/custom
 fi
 
+# Replace (next commit)
+# sed -i 's/$DEFAULTSCRIPTPATH/$SCRIPTPATH/g' Astor.desktop.tpl > Astor.desktop
+
 cp ./Astor.png /usr/share/icons/custom/
-cp ./Astor.desktop /usr/share/applications/
-chmod g-x /usr/share/applications/Astor.desktop
-chmod o-x /usr/share/applications/Astor.desktop
+cp ./Astor.desktop $APPLICATIONSDIR
+
+
+chmod g-x $APPLICATIONSDIR/Astor.desktop
+chmod o-x $APPLICATIONSDIR/Astor.desktop
 
 cp ./Jive.png /usr/share/icons/custom/
-cp ./Jive.desktop /usr/share/applications/
-chmod g-x /usr/share/applications/Jive.desktop
-chmod o-x /usr/share/applications/Jive.desktop
+cp ./Jive.desktop $APPLICATIONSDIR/
+
+chmod g-x $APPLICATIONSDIR/Jive.desktop
+chmod o-x $APPLICATIONSDIR/Jive.desktop
 
 cp ./Trends.png /usr/share/icons/custom/
-cp ./Trends.desktop /usr/share/applications/
-chmod g-x /usr/share/applications/Trends.desktop
-chmod o-x /usr/share/applications/Trends.desktop
+cp ./Trends.desktop $APPLICATIONSDIR/
+
+chmod g-x $APPLICATIONSDIR/Trends.desktop
+chmod o-x $APPLICATIONSDIR/Trends.desktop
 
 echo "Installation successful"
